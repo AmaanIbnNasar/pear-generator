92c8939350c5a95f0ce6d075a477e43a15566793
Amaan <amaan5260@gmail.com>
2021-10-22 12:37:46 +0100

    Working pairer

---
+++ __tests__/test_main.py
@@ -0,0 +1,38 @@
+import itertools
+from unittest import TestCase
+
+import networkx as nx
+
+from main import convertPreviousPairsToGraphWeights
+
+
+def setupGraph():
+    graph = nx.Graph()
+    graph.add_edges_from(itertools.combinations(range(1, 5), 2), weight=100)
+    return graph
+
+
+class Test(TestCase):
+    def test_basic_graph_gets_converted(self):
+        graph = setupGraph()
+        convertPreviousPairsToGraphWeights(graph, [])
+        self.assertEqual([(1, 2, 100), (1, 3, 100), (1, 4, 100), (2, 3, 100), (2, 4, 100), (3, 4, 100)],
+                         list(graph.edges.data("weight")))
+
+    def test_correctly_discounts_one_pair(self):
+        graph = setupGraph()
+        convertPreviousPairsToGraphWeights(graph, [{1:2}])
+        self.assertEqual([(1, 2, 0), (1, 3, 100), (1, 4, 100), (2, 3, 100), (2, 4, 100), (3, 4, 100)],
+                         list(graph.edges.data("weight")))
+
+    def test_correctly_discounts_one_pair_over_multiple_weeks(self):
+        graph = setupGraph()
+        convertPreviousPairsToGraphWeights(graph, [{1:2},{2:3}])
+        self.assertEqual([(1, 2, 0), (1, 3, 100), (1, 4, 100), (2, 3, 25), (2, 4, 100), (3, 4, 100)],
+                         list(graph.edges.data("weight")))
+
+    def test_correctly_adds_apple_bonus(self):
+        graph = setupGraph()
+        convertPreviousPairsToGraphWeights(graph, [{1:2},{2:0}])
+        self.assertEqual([(1, 2, 5), (1, 3, 100), (1, 4, 100), (2, 3, 105), (2, 4, 105), (3, 4, 100)],
+                         list(graph.edges.data("weight")))
--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-import networkx, json
+import networkx as nx, json, itertools, matplotlib.pyplot as plt
 
 def getPeople():
     with open("./__pairfiles__/people.json") as f:
         people = json.load(f)
-    return people
+    return people["people"]
 
 def getPreviousPairs():
     with open("./__pairfiles__/previous_pears.JSON") as f:
-        previousPairs = json.load(f)
-    return previousPairs
+        previousPairWeeks = json.load(f)
+    return previousPairWeeks
+
+def convertPeopleToGraph(people):
+    peopleGraph = nx.Graph()
+    peopleEdges = itertools.combinations(people, 2)
+    peopleGraph.add_edges_from(peopleEdges, weight=100)
+    return peopleGraph
+
+def convertPreviousPairsToGraphWeights(peopleGraph, previousPairWeeks):
+    for i, previousPairWeek in enumerate(previousPairWeeks):
+        for p1, p2 in previousPairWeek.items():
+            if p1 in peopleGraph.nodes and p2 == 0:
+                for p1Neigh in peopleGraph.neighbors(p1):
+                    peopleGraph.edges[p1, p1Neigh]["weight"] += 5
+                continue
+            if p1 not in peopleGraph.nodes or p2 not in peopleGraph.nodes:
+                # print("WARNING")
+                continue
+            peopleGraph.edges[p1, p2]["weight"] *= 0.5 - 0.5**(i+1)
+
+
+def plotGraph(graph):
+    pos = nx.drawing.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_edges(graph, pos, width=[edge[2]/100 for edge in graph.edges.data("weight")])
+    nx.draw_networkx_labels(graph, pos)
+    plt.show()
 
 def main():
     people = getPeople()
-    previousPairs = getPreviousPairs()
-    
+    previousPairWeeks = getPreviousPairs()
+    peopleGraph = convertPeopleToGraph(people)
+    convertPreviousPairsToGraphWeights(peopleGraph, previousPairWeeks)
+    plotGraph(peopleGraph)
+    print(nx.max_weight_matching(peopleGraph))
+
+
 
 
 if __name__=="__main__":
